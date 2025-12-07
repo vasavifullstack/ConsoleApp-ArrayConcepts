1bb0d413f7b8b66da1e9af072f325d552ab7d133
vasavifullstack <vasavi.fullstack@gmail.com>
2025-12-07 15:27:04 +0530

    hashtable

---
+++ ConsoleApp-ArrayConcepts/8.hashtable.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Csharp_Dec_Step4
+{
+    class HashtableExample
+    {
+        static void Main()
+        {
+
+            //hashtable-non generic key-value pair.
+            //it is not typesafe.so need to cast the values when retrieving them.
+            //it can be any type and dulplicates.
+
+            Hashtable studentDetails = new Hashtable();
+            studentDetails.Add("Name", "Alice");  //dictentry
+            studentDetails.Add("Age", 25);
+            studentDetails.Add("Location", "New York");
+            studentDetails.Add("IsStudent", true);  //a
+
+
+            //Fetch the value 
+
+
+            //object ---> object into respective type like below called boxing 
+
+            //string  ---> object called unboxing...
+
+            string name = (string)studentDetails["Name"];    //As a madan seeing the code and saying the value i.e. Alice
+
+            //step 1
+            //var name = studentDetails["Name"];
+
+            // Console.WriteLine(name);  //fetch
+
+            //foreach(DictionaryEntry item in studentDetails)
+            //{
+            //    //Console.WriteLine(item.Value);
+            //    //  Console.WriteLine(item.Key);
+
+            //    Console.WriteLine(item.Key + " : " + item.Value);
+            //}
+
+
+
+            studentDetails.Remove("Name");  //r
+
+            studentDetails["Age"] = 30;  //u
+
+
+
+
+            //foreach (DictionaryEntry item in studentDetails)
+            //{
+            //    //Console.WriteLine(item.Value);
+            //    //  Console.WriteLine(item.Key);
+
+            //    Console.WriteLine(item.Key + " : " + item.Value);
+            //}
+
+
+
+            studentDetails.Clear();  //erase
+            foreach (DictionaryEntry item in studentDetails)
+            {
+                //Console.WriteLine(item.Value);
+                //  Console.WriteLine(item.Key);
+
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+
+            Console.WriteLine();
+
+        }
+
+    }
+}
