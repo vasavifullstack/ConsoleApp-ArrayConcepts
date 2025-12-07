461eca7cfbcf7f859e7a6af0da1a877c14d912f0
vasavifullstack <vasavi.fullstack@gmail.com>
2025-12-07 16:35:24 +0530

    hashset

---
+++ ConsoleApp-ArrayConcepts/9.hashset.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Csharp_Dec_Step4
+{
+    class HashsetExample
+    {
+        //hashset-stores unique values and not allow duplicates.
+        //unordered collection-without any specific order unlike list or array.
+        static void Main()
+        {
+            HashSet<string> userNames = new HashSet<string>();
+
+            // Adding items to the HashSet
+            userNames.Add("Alice");
+            userNames.Add("Bob");
+            userNames.Add("Charlie");
+
+
+            userNames.Add("Alice");
+
+
+            // Checking if an item exists in the HashSet
+            Console.WriteLine("Is 'Alice' in the HashSet? " + userNames.Contains("Alice"));
+
+
+            foreach (string name in userNames)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            userNames.Remove("Bob");
+
+            // Checking if an item exists in the HashSet
+            Console.WriteLine("Is 'Alice' in the HashSet? " + userNames.Contains("Bob"));
+
+            Console.WriteLine();
+
+        }
+    }
+}
